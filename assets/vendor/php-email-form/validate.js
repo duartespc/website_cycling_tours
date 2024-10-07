29e9d6b143186d6135b6b874103a83f7f40f2dd3
duarte-21805799 <duarte.cambra@gmail.com>
2024-10-07 21:50:45 +0100

    try to remove error again

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -45,6 +45,7 @@
     })
     .then(data => {
       thisForm.querySelector('.loading').classList.remove('d-block');
+      thisForm.querySelector('.error-message').classList.remove('d-block');
       thisForm.querySelector('.sent-message').classList.add('d-block');
       thisForm.reset();
     })
